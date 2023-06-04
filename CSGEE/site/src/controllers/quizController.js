e43cd6c3e65d65bed2e17ed5faf2e95e77df3e60
RaphaelKlein <raphael.almeida@sptech.school>
2023-06-04 01:27:50 -0300

    o pai manja muito slk

--- CSGEE/site/src/controllers/quizController.js
+++ CSGEE/site/src/controllers/quizController.js
@@ -1,26 +1,28 @@
 var quizModel = require("../models/quizModels");
 
 function cadastrar(req, res) {
-    
+
     var acertos = req.body.acertosServer;
-    var erros = req.body.errosServer;  
+    var erros = req.body.errosServer;
     var fkUsuario = req.body.usuarioServer;
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        quizModel.cadastrar(acertos, erros, fkUsuario) 
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    quizModel.cadastrar(acertos, erros, fkUsuario)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
 }
 
+module.exports = {
+    cadastrar
+}
--- CSGEE/site/src/models/quizModels.js
+++ CSGEE/site/src/models/quizModels.js
@@ -1,7 +1,7 @@
 var database = require("../database/config")
 
 function cadastrar(acertos, erros, fkUsuario) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", acertos, erros, fkUsuario);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
